e80e7f20bfc5c9e0bea758763631b6f65c9207cb
smachewAdela <samadela@gmail.com>
2021-04-26 20:34:30 +0300

    overhaul started.

--- QBalanceDesktop/Form1.cs
+++ QBalanceDesktop/Form1.cs
@@ -16,12 +16,12 @@ namespace QBalanceDesktop
         public BaseForm()
         {
             InitializeComponent();
-            ProfileManager.LoadProfile();
+            //ProfileManager.LoadProfile();
         }
 
         private void BaseForm_Load(object sender, EventArgs e)
         {
-            LoadCombos();
+            //LoadCombos();
         }
 
         private void LoadCombos()
@@ -66,68 +66,68 @@ namespace QBalanceDesktop
          
         private void btnSync_Click(object sender, EventArgs e)
         {
-            SyncManager.Sync();
+            //SyncManager.Sync();
             MessageBox.Show("Sync Completed !");
         }
 
         private void btnScan_Click(object sender, EventArgs e)
         {
-            var items = Db.GetData<Transaction>(new SearchParameters());
-            foreach (var tran in items)
-            {
-                var cat = new DCategory
-                {
-                    Code = tran.category,
-                    Description = tran.CatDesc,
-                    GroupId = Convert.ToInt32( tran.CatGroupID)
-                };
-                var exs = Db.GetSingle<DCategory>(new SearchParameters { CategoryCode = cat.Code });
-                if (exs == null)
-                    Db.Insert(cat);
+            //var items = Db.GetData<Transaction>(new SearchParameters());
+            //foreach (var tran in items)
+            //{
+            //    var cat = new DCategory
+            //    {
+            //        Code = tran.category,
+            //        Description = tran.CatDesc,
+            //        GroupId = Convert.ToInt32( tran.CatGroupID)
+            //    };
+            //    var exs = Db.GetSingle<DCategory>(new SearchParameters { CategoryCode = cat.Code });
+            //    if (exs == null)
+            //        Db.Insert(cat);
 
-                var group = new CategoryGroup
-                {
-                    Id = cat.GroupId,
-                    Name = tran.CatGroup
-                };
-                var gxs = Db.GetSingle<CategoryGroup>(new SearchParameters { CategoryGroupName = group.Name });
-                if (gxs == null)
-                    Db.Insert(group);
-            }
+            //    var group = new CategoryGroup
+            //    {
+            //        Id = cat.GroupId,
+            //        Name = tran.CatGroup
+            //    };
+            //    var gxs = Db.GetSingle<CategoryGroup>(new SearchParameters { CategoryGroupName = group.Name });
+            //    if (gxs == null)
+            //        Db.Insert(group);
+            //}
 
-            var months = Db.GetData<TrackedMonth>(new SearchParameters()).ToDictionary(x => x.StartDate, x => x);
-            foreach (var month in months)
-            {
-                var monthTransactions = Db.GetData<Transaction>(new SearchParameters { TranFromDate = month.Value.StartDate, TranToDate = month.Value.StartDate.AddMonths(1) });
-                if (monthTransactions.Count > 0)
-                {
-                    var groupTrans = monthTransactions.GroupBy(x => x.CatGroupID).ToList();
-                    var catTrans = monthTransactions.GroupBy(x => x.category).ToList();
+            //var months = Db.GetData<TrackedMonth>(new SearchParameters()).ToDictionary(x => x.StartDate, x => x);
+            //foreach (var month in months)
+            //{
+            //    var monthTransactions = Db.GetData<Transaction>(new SearchParameters { TranFromDate = month.Value.StartDate, TranToDate = month.Value.StartDate.AddMonths(1) });
+            //    if (monthTransactions.Count > 0)
+            //    {
+            //        var groupTrans = monthTransactions.GroupBy(x => x.CatGroupID).ToList();
+            //        var catTrans = monthTransactions.GroupBy(x => x.category).ToList();
 
-                    foreach (var gt in groupTrans)
-                    {
-                        var catGStats = new GroupMonthlyTotal
-                        {
-                            Month = month.Value.Title,
-                            Name = gt.First().CatGroup,
-                            Amount = (int)gt.Sum(x => x.Amount)
-                        };
-                        Db.Insert(catGStats);
-                    }
+            //        foreach (var gt in groupTrans)
+            //        {
+            //            var catGStats = new GroupMonthlyTotal
+            //            {
+            //                Month = month.Value.Title,
+            //                Name = gt.First().CatGroup,
+            //                Amount = (int)gt.Sum(x => x.Amount)
+            //            };
+            //            Db.Insert(catGStats);
+            //        }
 
-                    foreach (var catTran in catTrans)
-                    {
-                        var catGStats = new CategoryMonthlyTotal
-                        {
-                            Month = month.Value.Title,
-                            Name = catTran.First().CatDesc,
-                            Amount = (int)catTran.Sum(x => x.Amount)
-                        };
-                        Db.Insert(catGStats);
-                    }
+            //        foreach (var catTran in catTrans)
+            //        {
+            //            var catGStats = new CategoryMonthlyTotal
+            //            {
+            //                Month = month.Value.Title,
+            //                Name = catTran.First().CatDesc,
+            //                Amount = (int)catTran.Sum(x => x.Amount)
+            //            };
+            //            Db.Insert(catGStats);
+            //        }
 
-                }
-            }
+            //    }
+            //}
 
 
             MessageBox.Show("Scan Completed !");
@@ -135,7 +135,7 @@ namespace QBalanceDesktop
 
         private void btnMigrate_Click(object sender, EventArgs e)
         {
-            MigrationManager.MigrateData();
+            //MigrationManager.MigrateData();
         }
     }
 }
